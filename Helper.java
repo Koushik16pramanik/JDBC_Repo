283b9e65e027c6457c24857c8ca7dfac64fc8ca7
Koushik Pramanik <110465853+Koushik16pramanik@users.noreply.github.com>
2022-12-03 08:02:24 +0530

    Add files via upload

---
+++ Helper.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Helper {
+		static {
+			try {
+				// register my sql driver
+				Class.forName("com.mysql.cj.jdbc.Driver");
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+		public static Connection con() throws SQLException {
+			return DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "Koushik@123");
+		}
+}
